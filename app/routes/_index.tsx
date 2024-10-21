f27d1d4d5dec6915cd1e706446f9940ce07306a5
Akshay Bharath <akshay23@users.noreply.github.com>
2024-10-20 23:59:25 -0400

    Clean up index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,18 +9,28 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Go SPURS Go</h1>
-      <ul>
-        <li>
+    <div style={{
+      fontFamily: "system-ui, sans-serif",
+      lineHeight: "1.8",
+      textAlign: "center",
+      fontSize: "1.2rem",
+      maxWidth: "800px",
+      margin: "0 auto",
+      padding: "20px"
+    }}>
+      <h1 style={{ fontSize: "2.5rem" }}>Under construction</h1>
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        <p style={{ fontSize: "1.5rem" }}>
+        In the meantime,&nbsp;
           <a
             target="_blank"
             href="https://www.youtube.com/watch?v=-OvP4UfNKD4&list=PLFW6X-ryKUlG0va1tzgP61VVjku4Liflk"
             rel="noreferrer"
           >
-            Tim Duncan - Clutch Moments!
+            here
           </a>
-        </li>
+          &nbsp;are some Tim Duncan clutch moments. Enjoy!
+        </p>
       </ul>
     </div>
   );
