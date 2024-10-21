a924b361ba4a3f6a0ed02aafdfc2bb7900679ccb
Akshay Bharath <akshay23@users.noreply.github.com>
2024-10-21 00:33:26 -0400

    Under construction image

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -19,22 +19,25 @@ export default function Index() {
       padding: "20px"
     }}>
       {/* Add an image before the heading */}
-      <img src="https://e7.pngegg.com/pngimages/336/644/png-clipart-under-construction-under-construction.png" alt="Construction" style={{ maxWidth: "100%", marginBottom: "20px" }} />
-      <h1 style={{ fontSize: "2.5rem" }}>Under Construction</h1>
-      <img src="https://e7.pngegg.com/pngimages/336/644/png-clipart-under-construction-under-construction.png" alt="Construction" style={{ maxWidth: "100%", marginBottom: "20px" }} />
-      <ul style={{ listStyleType: "none", padding: 0 }}>
-        <p style={{ fontSize: "1.5rem" }}>
-        In the meantime,&nbsp;
-          <a
-            target="_blank"
-            href="https://www.youtube.com/watch?v=-OvP4UfNKD4&list=PLFW6X-ryKUlG0va1tzgP61VVjku4Liflk"
-            rel="noreferrer"
-          >
-            here
-          </a>
-          &nbsp;are some Tim Duncan clutch moments. Enjoy!
+      <img 
+        src="https://media.istockphoto.com/id/1411798446/vector/modern-coming-soon-under-construction-sticker-banner.jpg?s=612x612&w=0&k=20&c=KgQ9eYEfuzTdmc5ypmMYoz8JHPTbe_TjEXxyxNixKQ4=" 
+        alt="Construction" 
+        style={{ 
+          width: "100%",  // Set your desired width
+          height: "100%" // Set your desired height
+        }} 
+      />
+      <p style={{ fontSize: "1.5rem" }}>
+      In the meantime,&nbsp;
+        <a
+          target="_blank"
+          href="https://www.youtube.com/watch?v=-OvP4UfNKD4&list=PLFW6X-ryKUlG0va1tzgP61VVjku4Liflk"
+          rel="noreferrer"
+        >
+          here
+        </a>
+        &nbsp;are some Tim Duncan clutch moments. Enjoy!
         </p>
-      </ul>
     </div>
   );
 }
